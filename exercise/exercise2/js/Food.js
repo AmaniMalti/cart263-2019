abaedb33aa54c10e6a0c866052d4ec2e511f2017
Amani <amanimalti.95@gmail.com>
2019-01-23 00:10:19 -0500

    final

--- exercise/exercise2/js/Food.js
+++ exercise/exercise2/js/Food.js
@@ -5,13 +5,14 @@
 
 class Food extends Agent {
 
+
   // Constructor
   //
   // Pass arguments on to the super() constructor (e.g. for Agent)
   // Also set a minimum and maximum size for this food object which it
   // will vary between when it resets
-  constructor(x,y,minSize,maxSize,vx,vy) {
-    super(x,y,random(minSize,maxSize),'#55cccc');
+  constructor(x,y,minSize,maxSize,color,vx,vy) {
+    super(x,y,random(minSize,maxSize),color);
     this.minSize = minSize;
     this.maxSize = maxSize;
     this.vx = vx;
@@ -23,61 +24,29 @@ class Food extends Agent {
   // Set position to a random location on the canvas
   // Set the size to a random size within the limits
   reset() {
-    this.x = random(0,width);
-    this.y = random(0,height);
-    this.size = random(this.minSize,this.maxSize);
+    //this.x = random(0,width);
+    //this.y = random(0,height);
+    //this.size = random(this.minSize,this.maxSize);
+	this.active = false;
   }
   // update()
   //
   update(){
 
-    const maxVX = 20;
-    const maxVY = 20;
-
     if (!this.active) {
       return;
     }
 
+  // Update position with velocity and constrain position to be on screen
+  //this.vx = this.vx + random(-maxSpeed,maxSpeed),(-maxSpeed,maxSpeed);
+	//this.vy = this.vy + random(-maxSpeed,maxSpeed),(-maxSpeed,maxSpeed);
+  const maxSpeed = 4;
+  this.vx = constrain(this.vx + random(-maxSpeed,maxSpeed),-maxSpeed,maxSpeed);
+  this.vy = constrain(this.vy + random(-maxSpeed,maxSpeed),-maxSpeed,maxSpeed);
 
-  // Update position with velocity
-  this.x += this.vx;
-  this.y += this.vy;
+	this.x = constrain(this.x + this.vx,0,width);
+  this.y = constrain(this.y + this.vy,0,height);
+	}
 
-// Constrain y position to be on screen
-//this.y = constrain(this.y,0,height-this.size);
 
-// Check for touching upper or lower edge and reverse velocity if so
-  /*if (this.y === 0 || this.y + this.size === height) {
-      this.vy = -this.vy;
-  }
-  else {
-      this.vy += random(0,maxVY);
-  }
-  if (this.x + this.size < 0 || this.x > width) {
-     this.vx = -this.vx;
-  }
-  else {
-      this.vx += random(0,maxVX);
-  }*/
-      if (this.x < 0) {
-        this.x = width;
-      }
-      else if (this.x > width) {
-                this.x = 0;
-    }
-      if (this.y < 0) {
-           this.y = height;
-    }
-      else if (this.y > height) {
-                this.y = 0;
-    }
-  }
-
-  calculateVelocity(){
-    this.vx += random(-2,2);
-    this.vy += random(-2,2);
-    this.vy = constrain(this.vy, -4,4);
-    this.vx = constrain(this.vx,-4,4);
-
-  }
 }
--- exercise/exercise2/js/script.js
+++ exercise/exercise2/js/script.js
@@ -1,16 +1,12 @@
 "use strict";
 
-/*****************
 
-OOP Circle Eater
-Pippin Barr
 
-An Object-Oriented version of the Circle Eater program.
-The player moves a circle around with the mouse.
-Another circle represents food which the player eats by overlapping.
-The player circle shrinks over time, but grows when it eats.
+//An Object-Oriented version of the Circle Eater program.
+//The player moves a circle around with the mouse.
+//Another circle represents food which the player eats by overlapping.
+//The player circle shrinks over time, but grows when it eats.
 
-******************/
 
 // Constants for key quantities
 const AVATAR_MAX_SIZE = 64;
@@ -40,11 +36,10 @@ function preload() {
 function setup() {
   createCanvas(windowWidth,windowHeight);
   avatar = new Avatar(mouseX,mouseY,AVATAR_MAX_SIZE,AVATAR_SIZE_LOSS_PER_FRAME);
-  for (let i = 0; i < 20; i++){
-    foods.push(new Food(random(0,width),random(0,height),FOOD_MIN_SIZE,FOOD_MAX_SIZE,2,2));
+  for (let i = 0; i < 42; i++){
+    foods.push(new Food(random(0,width),random(0,height),FOOD_MIN_SIZE,FOOD_MAX_SIZE,color(random(210),random(210),random(210)),2,2));
   }
-  //food = new Food(random(0,width),random(0,height),FOOD_MIN_SIZE,FOOD_MAX_SIZE,2,2);
-  noCursor();
+   noCursor();
 }
 
 
@@ -56,22 +51,18 @@ function setup() {
 
 function draw() {
   background(0);
-
-  //food.update();
-
   avatar.update();
+  //check for collision
   for (let i = 0; i < foods.length; i++) {
-  if (avatar.collide(foods[i])) {
-    avatar.eat(foods[i]);
-    break;
-  }
-}
+	if (avatar.collide(foods[i])) {
+		avatar.eat(foods[i]);
+		break;
+		}
+	}
   avatar.display();
-  //food.display();
+  //Display food
   for (let i = 0; i < foods.length; i++) {
-    // Moving the balls randomly (as the perlin noise algorithme)
-  foods[i].calculateVelocity();
-  foods[i].update();
-  foods[i].display();
-}
+	foods[i].update();
+	foods[i].display();
+	}
 }
