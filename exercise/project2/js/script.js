febee8cf69395f769619805c2069f79dd002baef
Amani <amanimalti.95@gmail.com>
2019-03-13 00:38:36 -0400

    P2: 3. Adding a song to play in the background
    Adding instructions to the game.
    Adding a song in the background to play in loop.

--- exercise/project2/js/script.js
+++ exercise/project2/js/script.js
@@ -10,3 +10,18 @@ Shark is waiting for the baby to get to the bottom to eat it.
 Needs to shout Stop to stop the shark from eating the baby.
 
 ******************/
+
+// Sound effects for the experience
+let song = new Audio("assets/sounds/babyshark.mp3");
+
+// adding variables
+
+
+$(document).ready(setup);
+
+function setup() {
+
+  }
+// song is playing during the whole game
+song.loop = true;
+song.play();
