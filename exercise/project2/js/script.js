d40ae065607c32d18a3cbf8077486c90d46db1cf
Amani <amanimalti.95@gmail.com>
2019-03-13 21:34:04 -0400

    P2: 4.Adding variables and a new image
    1)Adding variables of images in script.
    2)Adding new image of shark eating the baby in html & css.

--- exercise/project2/js/script.js
+++ exercise/project2/js/script.js
@@ -12,16 +12,19 @@ Needs to shout Stop to stop the shark from eating the baby.
 ******************/
 
 // Sound effects for the experience
-let song = new Audio("assets/sounds/babyshark.mp3");
+let babySong = new Audio("assets/sounds/babyshark.mp3");
 
 // adding variables
-
+let shark;
+let hungryShark;
+let sharkEat;
+let baby;
 
 $(document).ready(setup);
 
 function setup() {
 
-  }
-// song is playing during the whole game
-song.loop = true;
-song.play();
+}
+//song is playing during the whole game
+babySong.loop = true;
+babySong.play();
