018d130e9a7578323cd965b869714ea6db859bab
Amani <amanimalti.95@gmail.com>
2019-03-16 19:59:49 -0400

    P2: 12. Added annyang
    1) added annyang link in html and added annyang library in javascript folder.
    2) added annywang function to stop the shark from eating the baby and made it disapear

--- exercise/project2/js/script.js
+++ exercise/project2/js/script.js
@@ -11,93 +11,98 @@ Needs to shout Stop to stop the shark from eating the baby.
 
 ******************/
 
-$(document).ready(setup);
 // Sound effects for the experience
 let babySong = new Audio("assets/sounds/babyshark.mp3");
 
-// song is playing during the whole game
-babySong.loop = true;
-let songPlay = babySong.play();
-if (songPlay !== undefined) {
-    songPlay.then(_ => {
+// adding variables
+	let shark;
+	let hungryShark;
+	let sharkEat;
+	let baby;
+	let saveBaby;
 
-    })
-    .catch(error => {
-      console.log('Could not play song');
-    });
-  }
+$(document).ready(function() {
+	// more variables to calculate width and height of elements
+	let babyHeight = $('#baby').height();
+    let sharkHeight = $('#shark').height();
+    let windowHeight = $( document ).height();
+    let windowWidth = $( document ).width();
 
-function setup() {
 
-  // adding variables
-  let shark = $('#shark');
-  let hungryShark = $('#hungryShark');
-  let sharkEat = $('#sharkEat')
-  let baby = $('#baby');
-
-  // more variables to calculate width and height of elements
-  let babyHeight = $('#baby').height();
-  let sharkHeight = $('#shark').height();
-  let windowHeight = $( document ).height();
-  let windowWidth = $( document ).width();
-
-  // position the baby horizontally
+	// function to position the baby horizontally
   function setupBabyX(xPosition){
-    baby.css({'left': xPosition +'px'});
+    $('#baby').css({'left': xPosition +'px'});
   };
-
-  // animate the movement of the baby vertically
+	// function to animate the movement of the baby vertically
   function animateBabyY(yPosition){
-    baby.animate({top: yPosition + 'px' }, {easing: 'swing', duration: 2500,  complete: function() {animateSharkEatsBaby(yPosition, babyHeight, sharkHeight)}})
+    $('#baby').animate({top: yPosition + 'px' }, {easing: 'swing', duration: 2500,  complete: function() {animateSharkEatsBaby(yPosition, babyHeight, sharkHeight)}})
   };
-
-  // animate the shark movement along the x position
+	// animate the shark movement along the x position
   function animateShark(xPosition) {
-    shark.animate({left: xPosition +'px'}, 2000);
+    $('#shark').animate({left: xPosition +'px'}, 2000);
   };
-
-  // generate a random number along the width of the page for the horizontal positioning
+	// generate a random number along the width of the page for the horizontal positioning
   function generateRandomXPosition() {
    return Math.floor(Math.random() * windowWidth - 30);
   };
-
-  // generate a random number along the height of the page for the vertical positioning
+	// generate a random number along the height of the page for the vertical positioning
   function generateRandomYPosition() {
    return Math.floor(Math.random() * windowHeight);
   }
-
-  // calculate height of baby element
+	// calculate height of baby element
   function getBabyHeight() {
-    return baby.css('top');
+    return $('#baby').css('top');
   }
-
-  // animate the shark eating baby
+	// animate the shark eating baby
   function animateSharkEatsBaby(yPosition, babyHeight, sharkHeight) {
-    shark.hide();
-      hungryShark.css('left', shark.css('left'));
-      hungryShark.show();
-      baby.animate({top: yPosition + 'px' }, {easing: 'swing', duration: 1500,  complete: function() {animateSharkAteBaby(yPosition, babyHeight, sharkHeight)}});
+      $('#shark').hide();
+      $('#hungryShark').css('left', $('#shark').css('left'));
+      $('#hungryShark').show();
+      $('#baby').animate({top: yPosition + 'px' }, {easing: 'swing', duration: 1500,  complete: function() {animateSharkAteBaby(yPosition, babyHeight, sharkHeight)}});
   }
-
-  // animate shark opening mouth and baby dropping into shark's mouth
+	// animate shark opening mouth and baby dropping into shark's mouth
   function animateSharkAteBaby(yPosition, babyHeight, sharkHeight) {
-    baby.animate({top: yPosition + babyHeight * 2 + 'px'}, {easing: 'swing', duration: 500,  complete: function() {animateBabyEaten()}});
+	  console.log(saveBaby);
+	  if (!saveBaby) {
+		console.log("SHARK ATE THE BABY!!!");
+		$('#baby').animate({top: yPosition + babyHeight * 2 + 'px'}, {easing: 'swing', duration: 500,  complete: function() {animateBabyEaten()}});
+	  }
+	  else {
+		 console.log("BABY SAVED!!!");
+		 $('#hungryShark').hide();
+	  }
   }
-
-  // transition to baby eaten image
+	// transition to baby eaten image
   function animateBabyEaten() {
-    hungryShark.hide();
-    sharkEat.css('left', hungryShark.css('left'));
-    baby.hide();
-    sharkEat.show();
+		$('#hungryShark').hide();
+		$('#sharkEat').css('left', $('#hungryShark').css('left'));
+		$('#baby').hide();
+		$('#sharkEat').show();
   }
 
-  // generate a horizontal position for the baby
-  var babyXPosition = generateRandomXPosition();
-  // set the baby horizontal position
-  setupBabyX(babyXPosition);
-  // animate movement of shark to horizontal position
-  animateShark(babyXPosition);
-  // animate baby dropping to meet the shark vertically
-  animateBabyY(windowHeight - sharkHeight - babyHeight);
- };
+	// Adding speach STOP to stop the shark from eating the baby
+  if (annyang) {
+		saveBaby = false;
+		var command = {
+		'stop': function() {
+		saveBaby = true;
+			}
+		};
+		 annyang.addCommands(command);
+		 annyang.start();
+
+ 	 	// generate a horizontal position for the baby
+    var babyXPosition = generateRandomXPosition();
+		// set the baby horizontal position
+    setupBabyX(babyXPosition);
+		// animate movement of shark to horizontal position
+    animateShark(babyXPosition);
+		// animate baby dropping to meet the shark vertically
+    animateBabyY(windowHeight - sharkHeight - babyHeight);
+	}
+ });
+
+
+		// song is playing during the whole game
+		babySong.loop = true;
+		babySong.play();
